a23754e928e1d59f51cfb1f9260946258e9db5da
mark <269105294@qq.com>
2018-06-30 20:21:23 +0800

    2018.6.30

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 import { DiscoveryPage } from '../pages/discovery/discovery';
 import { ChatPage } from '../pages/chat/chat';
 import { NotificationPage } from '../pages/notification/notification';
 import { MorePage } from '../pages/more/more';
+import { LoginPage } from '../pages/login/login';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 import { RestProvider } from '../providers/rest/rest';
 
 @NgModule({
@@ -23,7 +25,8 @@ import { RestProvider } from '../providers/rest/rest';
     DiscoveryPage,
     ChatPage,
     NotificationPage,
-    MorePage
+    MorePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
@@ -37,7 +40,8 @@ import { RestProvider } from '../providers/rest/rest';
     DiscoveryPage,
     ChatPage,
     NotificationPage,
-    MorePage
+    MorePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -1,6 +1,8 @@
-import { Http } from '@angular/http';
+import { Http,Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 /*
   Generated class for the RestProvider provider.
@@ -15,6 +17,17 @@ export class RestProvider {
     console.log('Hello RestProvider Provider');
   }
 
+
+
+
+  /**
+   *全局获取HTTP请求的方法
+   *
+   * @private
+   * @param {string} url
+   * @returns {Observable<string[]>}
+   * @memberof RestProvider
+   */
   private getUrlReturn(url:string):Observable<string[]>{
     return this.http.get(url)
                     .map(this.extractData)
@@ -23,6 +36,19 @@ export class RestProvider {
   private extractData(res:Response){
     let body = res.json();
     return JSON.parse(body) || {};
+  }
+
+  private handleError(error:Response | any){
+    let errMsg:string;
+    if(error instanceof Response){
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    }else{
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
 }
